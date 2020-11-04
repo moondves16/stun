9c34ca1eeb9def59ccfdf3a35cea9f19ca3bda99
moondves16 <moondves45@gmail.com>
2020-11-04 10:06:19 +0900

    base code test03

--- flask/main.py
+++ flask/main.py
@@ -4,12 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-<<<<<<< HEAD
     return 'Hello, stun!!'
-=======
     return 'Hello, stun! + 준원'
->>>>>>> ee5afe40ab4908bf5e19ecc8821eb4bda2142945
+    return 'Hello, stun! + 환'
 
 if __name__ == '__main__':
     app.run()
-
